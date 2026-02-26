fc6271d1a25493facdc1cd6a5a9b3e2b3bd6d14d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-26 12:25:04 +0500

    Add 404 error handling in server.js: return a JSON error response for non-existent routes to improve API robustness.

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -9,6 +9,10 @@ const server = http.createServer(async (req, res) => {
         res.setHeader("Content-Type", "application/json")
         res.statusCode = 200;
         res.end(JSON.stringify(destinations))
+    } else {
+        res.setHeader("Content-Type", "application/json")
+        res.statusCode = 404;
+        res.end(JSON.stringify({ error: "not found", message: "The requested route does not exist" }))
     }
 })
 
