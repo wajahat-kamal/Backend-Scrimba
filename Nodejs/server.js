753c63958f147edd4f12b7a3a188d3358dfac424
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-27 12:35:22 +0500

    Enhance server.js to improve continent and country data filtering: update API logic to utilize getDataByPathParams for both continent and country queries, ensuring consistent response handling and code clarity.

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -11,12 +11,15 @@ const server = http.createServer(async (req, res) => {
         sendJSONResponse(res, 200, destinations)
 
     } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
-        // const continent = req.url.split("/").pop()
-        // const filteredData = destinations.filter((destination) => {
-        //     return destination.continent.toLowerCase() === continent.toLowerCase()
-        // })
-        
-        getDataByPathParams()
+        const continent = req.url.split("/").pop()
+
+        const filteredData = getDataByPathParams(destinations, "continent", continent)
+        sendJSONResponse(res, 200, filteredData)
+
+    } else if (req.url.startsWith("/api/country") && req.method === "GET") {
+        const country = req.url.split("/").pop()
+
+        const filteredData = getDataByPathParams(destinations, "country", country)
         sendJSONResponse(res, 200, filteredData)
     }
     else {
