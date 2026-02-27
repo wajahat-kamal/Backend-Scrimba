244ce33ff13d28470f56910de8dc0d123addb5f0
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-27 12:53:03 +0500

    Refactor server.js to improve API request handling: update URL parsing and filtering logic for destinations, enhancing clarity and maintainability of the code.

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,17 +1,23 @@
 import http from "node:http"
 import { getDataFromDB } from "./db.js";
 import { getDataByPathParams, sendJSONResponse } from "./utils.js";
-
 const PORT = 5000;
 
 const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB()
 
-    if (req.url === "/api" && req.method === "GET") {
-        sendJSONResponse(res, 200, destinations)
+    const urlObj = new URL(req.url, `http://${req.headers.host}`)
+    const queryObj = Object.fromEntries(urlObj.searchParams)
+
+    if (urlObj.pathname === "/api" && req.method === "GET") {
+        let filteredDestinations = destinations
+
+        console.log(urlObj);
+
+        sendJSONResponse(res, 200, filteredDestinations)
 
     } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
-        
+
         const continent = req.url.split("/").pop()
         const filteredData = getDataByPathParams(destinations, "continent", continent)
         sendJSONResponse(res, 200, filteredData)
