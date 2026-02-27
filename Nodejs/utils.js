9903f2e9616f538a69fa4958d72c6c5b4e337639
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-27 16:32:13 +0500

    Add CORS headers in sendJSONResponse function: enable cross-origin requests by setting Access-Control-Allow-Origin and Access-Control-Allow-Method headers, enhancing API compatibility.

--- Nodejs/utils.js
+++ Nodejs/utils.js
@@ -1,6 +1,8 @@
 
 export function sendJSONResponse(res, statusCode, payload) {
     res.setHeader("Content-Type", "application/json")
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Method", "GET")
     res.statusCode = statusCode
     res.end(JSON.stringify(payload))
 }
