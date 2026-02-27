93b7ddbb3c4da8adbd0440ff35df1dd504e9d078
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-27 12:46:40 +0500

    Refactor server.js for improved readability: add whitespace for better code organization in API endpoint handling, enhancing overall clarity without altering functionality.

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -11,14 +11,14 @@ const server = http.createServer(async (req, res) => {
         sendJSONResponse(res, 200, destinations)
 
     } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
+        
         const continent = req.url.split("/").pop()
-
         const filteredData = getDataByPathParams(destinations, "continent", continent)
         sendJSONResponse(res, 200, filteredData)
 
     } else if (req.url.startsWith("/api/country") && req.method === "GET") {
-        const country = req.url.split("/").pop()
 
+        const country = req.url.split("/").pop()
         const filteredData = getDataByPathParams(destinations, "country", country)
         sendJSONResponse(res, 200, filteredData)
     }
