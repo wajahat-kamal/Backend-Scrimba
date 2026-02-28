e8edf24d6f62a3c6306bca4e74db57a2b7851683
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-28 11:58:49 +0500

    Fix heading capitalization in server response: update HTML message to ensure consistent styling in the server's confirmation response.

--- Nodejs/Routes-Paths/server.js
+++ Nodejs/Routes-Paths/server.js
@@ -5,7 +5,7 @@ const PORT = 8000
 const server = http.createServer(async (req, res) => {
     if (req.url === "/" && req.method === "GET") {
         res.writeHead(200, {"Content-Type": "text/html"})
-        const heading = `<html><h1>The server is Working!</h1></html>`
+        const heading = `<html><h1>The Server is Working!</h1></html>`
         res.end(heading)
     }
 })
