cb67e7a0430b4a5478b26486d67d2f3d0f7b6392
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-28 11:40:31 +0500

    Add form validation and data preparation in upload-sighting.js: implement checks for required fields and format the date for improved user feedback and data handling during sighting uploads.

--- Nodejs/Routes-Paths/client/upload-sighting.js
+++ Nodejs/Routes-Paths/client/upload-sighting.js
@@ -3,4 +3,39 @@ const formMessageText = document.getElementsByClassName("form-message-text")[0]
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    const location = document.getElementById("location").value
+    const text = document.getElementById("details").value
+    const title = document.getElementById("title").value
+
+    if (!location || !text || !title) {
+        formMessageText.textContent = `Please complete all fields!`
+        return
+    }
+
+    const isoDateString = document.getElementById("datetime").value
+
+    if (!isoDateString) {
+        formMessageText.textContent = "Please select a date and time!"
+        return
+    }
+
+    const date = new Date(isoDateString)
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false, form
+    }
+
+    const readableDate = date.toLocaleString("en-GB", options)
+
+    const formData = {
+        timeStamp: readableDate,
+        text,
+        title,
+        location,
+    }
 })
