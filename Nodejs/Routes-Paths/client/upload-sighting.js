6b074e810657ac744b78e06e6e0c7461333b3b31
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-28 11:36:44 +0500

    Add form submission handling in upload-sighting.js: implement event listener to prevent default form submission behavior, enhancing user experience for sighting uploads.

---
+++ Nodejs/Routes-Paths/client/upload-sighting.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("eventForm")
+const formMessageText = document.getElementsByClassName("form-message-text")[0]
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
