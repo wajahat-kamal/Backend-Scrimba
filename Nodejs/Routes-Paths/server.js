1e49e371b69903f25604675040b01cdd45836efc
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-28 11:58:32 +0500

    Add basic route handling in server.js: implement a response for GET requests to the root URL, confirming server functionality with a simple HTML message.

--- Nodejs/Routes-Paths/server.js
+++ Nodejs/Routes-Paths/server.js
@@ -3,7 +3,11 @@ import http from "node:http"
 const PORT = 8000
 
 const server = http.createServer(async (req, res) => {
-
+    if (req.url === "/" && req.method === "GET") {
+        res.writeHead(200, {"Content-Type": "text/html"})
+        const heading = `<html><h1>The server is Working!</h1></html>`
+        res.end(heading)
+    }
 })
 
 server.listen(PORT, () => console.log(`Server runing on port: ${PORT}`))
