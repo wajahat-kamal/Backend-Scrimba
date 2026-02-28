fe012023b5bdd6c54ad674715d36982470f33aa5
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-28 11:24:35 +0500

    Enhance renderCards function in index.js: update the function to iterate over cardsData and generate HTML for each sighting card, improving the display of fetched data in the client-side application.

--- Nodejs/Routes-Paths/client/index.js
+++ Nodejs/Routes-Paths/client/index.js
@@ -6,7 +6,21 @@ try {
     console.log(error);
 }
 
-function renderCards(cardsData){
+function renderCards(cardsData) {
     const cardContainer = document.querySelector(".cards-container")
     let cardHTML = ""
+
+    cardsData.forEach((card, i) => {
+        cardHTML = `
+        <article class="sighting-card" aria-labelledby="sighting-title-${i}">
+            <p class="card-details">${card.timeStamp}, ${card.location}</p>
+            <h3 id="sighting-title-${i}">${card.title}</h3>
+            <div class="sighting-text-wrapper">
+                <p class="sighting-text">${card.text}</p>
+            </div>
+            <button class="read-more-btn" aria-expanded="false">Read in full</button>
+        </article>
+        `
+    });
+    cardContainer.innerHTML = cardHTML
 }
