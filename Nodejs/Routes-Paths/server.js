d5bfb8788dafc0209aaf3f7238f406d86134a821
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 06:40:00 +0500

    Update server.js to define __dirname and improve response header formatting: ensure consistent spacing in headers for better readability.

--- Nodejs/Routes-Paths/server.js
+++ Nodejs/Routes-Paths/server.js
@@ -1,10 +1,11 @@
 import http from "node:http"
 
 const PORT = 8000
+const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
     if (req.url === "/" && req.method === "GET") {
-        res.writeHead(200, {"Content-Type": "text/html"})
+        res.writeHead(200, { "Content-Type": "text/html" })
         const heading = `<html><h1>The Server is Working!</h1></html>`
         res.end(heading)
     }
